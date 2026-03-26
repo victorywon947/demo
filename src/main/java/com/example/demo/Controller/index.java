824b6a8c00e92943b5c513497bf82d0cc967df44
victorywon947 <caze2352@gmail.com>
2026-03-26 11:11:43 +0900

    add controller

---
+++ src/main/java/com/example/demo/Controller/index.java
@@ -0,0 +1,15 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller			// 서버 사용할 시, 이 파일을 찾을 수 있게끔 함
+public class index {
+	@GetMapping("/")		//localhost:8080 접속할 때
+	@ResponseBody			//아래의 내용으로 응답한다.
+	public String helloworld() {
+		return "Helloworld";
+	}
+	
+}
