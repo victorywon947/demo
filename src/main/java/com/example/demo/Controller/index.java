8d8bd07eabe16ce6c7d7f7577de942ef111b3820
victorywon947 <caze2352@gmail.com>
2026-03-27 14:14:59 +0900

    docker connect

--- src/main/java/com/example/demo/Controller/index.java
+++ src/main/java/com/example/demo/Controller/index.java
@@ -9,7 +9,7 @@ public class index {
 	@GetMapping("/")		//localhost:8080 접속할 때
 	@ResponseBody			//아래의 내용으로 응답한다.
 	public String helloworld() {
-		return "Please";
+		return "Docker 연동되었습니다";
 	}
 	
 }
